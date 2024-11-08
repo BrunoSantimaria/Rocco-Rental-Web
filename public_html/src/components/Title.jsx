e63ac614cfaa6f4eb0c3b9242acef51355ea4661
Bruno Santimaria <santimariabruno@gmail.com>
2024-11-08 12:47:58 -0300

    fix img

--- public_html/src/components/Title.jsx
+++ public_html/src/components/Title.jsx
@@ -1,4 +1,4 @@
-import Imagen from "../img/img_3053.jpg";
+const Imagen = "https://res.cloudinary.com/di92lsbym/image/upload/v1731080807/IMG_3053_kmc0lf.jpg";
 export const Title = () => {
   return (
     <div className='flex bg-black h-[840px] lg:w-5/6 lg:mx-auto w-full object-contain'>
@@ -13,17 +13,9 @@ export const Title = () => {
       <div className='absolute  mx-0 align-middle my-20 ml-5'>
         <section className='lg:my-20 '>
           <h1 className='absolute w-80 text-color3 tracking-xl text-4xl whitespace-pre-line font-josefin font-semibold ml-2 mt-16'>
-            Estamos para{" "}
-            <span className='text-color2'>
-              {`${"ayudarte"}\n`}{" "}
-            </span>{" "}
-            a hacer el trabajo.{"\n"}{" "}
+            Estamos para <span className='text-color2'>{`${"ayudarte"}\n`} </span> a hacer el trabajo.{"\n"}{" "}
             <div className='pt-10'>
-              <span className='text-color2'>
-                Rocco Rental
-              </span>{" "}
-              {"\n"}Alquiler de
-              plataformas y andamios.
+              <span className='text-color2'>Rocco Rental</span> {"\n"}Alquiler de plataformas y andamios.
             </div>
           </h1>
         </section>
